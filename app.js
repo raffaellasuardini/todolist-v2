a9f134cf81c15c3cb80c39c51a034c994a9858b2
raffaella <suardini.raffaella@gmail.com>
2023-01-11 11:33:08 +0100

    add delete route

--- app.js
+++ app.js
@@ -68,6 +68,19 @@ app.post("/", function (req, res) {
   res.redirect("/");
 });
 
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Succesfully deleted id item " + checkedItemId);
+      res.redirect("/");
+    }
+  });
+});
+
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
