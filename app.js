38b2e7238524753981ad33ca005f875f3c08834f
raffaella <suardini.raffaella@gmail.com>
2023-01-19 13:04:52 +0100

    removed lodash

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash");
 
 const app = express();
 
